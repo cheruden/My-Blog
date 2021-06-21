edcf43f716ee42c5b3a5cf0921610580c2f01a05
cheruden <cheruden25@gmail.com>
2021-06-21 09:21:18 +0300

    add configuration content to make my database communicate with my applicatio

---
+++ config.py
@@ -0,0 +1,38 @@
+import os
+
+
+class Config:
+    '''
+    General configuration parent class
+    '''
+
+    UPLOADED_PHOTOS_DEST = 'app/static/photos'
+
+    #  email configurations
+    MAIL_SERVER = 'smtp.gmail.com'
+    MAIL_PORT = 587
+    MAIL_USE_TLS = True
+    MAIL_USERNAME = os.environ.get("MAIL_USERNAME")
+    MAIL_PASSWORD = os.environ.get("MAIL_PASSWORD")
+
+ 
+
+class ProdConfig(Config):
+    SQLALCHEMY_DATABASE_URI = os.environ.get("DATABASE_URL","")
+    if SQLALCHEMY_DATABASE_URI.startswith("postgres://"):
+        SQLALCHEMY_DATABASE_URI =SQLALCHEMY_DATABASE_URI.replace("postgres://","postgresql://",1)
+
+class TestConfig(Config):
+    SQLALCHEMY_DATABASE_URI = 'postgresql+psycopg2://moringa:Deno123@localhost/blogger_test'
+
+
+class DevConfig(Config):
+    SQLALCHEMY_DATABASE_URI = 'postgresql+psycopg2://moringa:Deno123@localhost/blogger'
+    DEBUG = True
+
+
+config_options = {
+    'development': DevConfig,
+    'production': ProdConfig,
+    'test': TestConfig   
+}
